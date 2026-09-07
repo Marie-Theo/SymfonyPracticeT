1d33eecee730f5cbeb49c1f2211111bbd15de95c
marie <marie@sio.ndlp>
2026-09-07 15:59:00 +0200

    [Change] Welcome Page

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class WelcomeController extends AbstractController
 {
-    #[Route('/welcome', name: 'app_welcome')]
+    #[Route('/', name: 'app_welcome')]
     public function index(): Response
     {
         return $this->render('welcome/index.html.twig', [
